a54337373962b78ea157f3019362e00fb6d7d291
Paulo <pauloestrella1994@gmail.com>
2020-11-22 23:30:25 -0300

    Fix to run crawl, set -O to export in json and csv files [WIP]

--- mercado_livre/mercado_livre/spiders/mercado_livre.py
+++ mercado_livre/mercado_livre/spiders/mercado_livre.py
@@ -4,19 +4,16 @@ import scrapy
 class MercadoLivreSpider(scrapy.Spider):
     name = "mercado_livre"
 
-    def requests_pages(self):
-        urls = [
-            'https://lista.mercadolivre.com.br/olist',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = ['https://lista.mercadolivre.com.br/olist#D[A:olist]']
+
         
     def parse(self, response):
-        product_name = quote.css('.ui-search-item__title::text').getall()
-        prices = quote.xpath('//div[has-class("ui-search-price--size-medium")]//span[has-class("price-tag ui-search-price__part")]//span[has-class("price-tag-fraction")]/text()').getall()
+        product_name = response.css('.ui-search-item__title::text').getall()
+        prices = response.xpath('//div[has-class("ui-search-price--size-medium")]' +
+                                '//span[has-class("price-tag ui-search-price__part")]' + 
+                                '//span[has-class("price-tag-fraction")]/text()').getall()
+        yield {
+            'product_name': product_name,
+            'price': prices
+        }
 
-        for quote in response:
-            yield {
-                'product_name': product_name,
-                'price': prices,
-            }
